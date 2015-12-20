c89ab0e1d52aa90e49f09c10c5594468cac682fd
Peter Velkov <kidroca@gmail.com>
2015-12-21 01:48:51 +0200

    Created a Date Range model

---
+++ WebApi/FinanceTracker.WebApi.AspApi/Models/DateRanageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceTracker.WebApi.AspApi.Models
+{
+    using System;
+
+    public class DateRanageModel
+    {
+        public DateTime FromDate { get; set; }
+
+        public DateTime ToDate { get; set; }
+    }
+}
