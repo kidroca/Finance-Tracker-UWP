ce432205fd5f8f7379aa01668344c1e51589371f
Peter Velkov <kidroca@gmail.com>
2015-12-21 09:40:12 +0200

    Added a Transaction Type to Color converter

---
+++ UniversalApp/FinanceTracker.UniversalApp.UI/Helpers/TypeToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace FinanceTracker.UniversalApp.UI.Helpers
+{
+    using System;
+    using ViewModels;
+    using Windows.UI;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Media;
+
+    public class TypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType != typeof(SolidColorBrush))
+            {
+                return null;
+            }
+
+            TransactionType type;
+            if (Enum.TryParse(value.ToString(), out type))
+            {
+                return null;
+            }
+
+            if (type == TransactionType.Deposit)
+            {
+                return new SolidColorBrush(Colors.LawnGreen);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Crimson);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
