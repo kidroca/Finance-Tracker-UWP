6239fa03e8787d630b4d355d47f1814b3e92774f
Peter Velkov <kidroca@gmail.com>
2015-12-24 12:05:22 +0200

    Updated Transaction Model in the UI project

--- UniversalApp/FinanceTracker.UniversalApp.UI/Data/Models/Transactions/TransactionModel.cs
+++ UniversalApp/FinanceTracker.UniversalApp.UI/Data/Models/Transactions/TransactionModel.cs
@@ -2,6 +2,7 @@ namespace FinanceTracker.UniversalApp.UI.Data.Models.Transactions
 {
     using System;
     using System.ComponentModel.DataAnnotations;
+    using Newtonsoft.Json;
 
     public class TransactionModel
     {
@@ -14,6 +15,11 @@ namespace FinanceTracker.UniversalApp.UI.Data.Models.Transactions
         [Display(Name = "Date")]
         public DateTime DateTime { get; set; }
 
+        [Required]
+        [MinLength(2, ErrorMessage = "Transaction name should be at least 2 characters long")]
+        [MaxLength(128, ErrorMessage = "Transaction name should be less than 128 characters")]
+        public string Name { get; set; }
+
         [Range(
             0,
             double.MaxValue,
@@ -23,6 +29,9 @@ namespace FinanceTracker.UniversalApp.UI.Data.Models.Transactions
 
         public TransactionType Type { get; set; }
 
+        [MinLength(2, ErrorMessage = "Transaction notes should be at least 2 characters long")]
+        [MaxLength(1000, ErrorMessage = "Transaction notes should be no longer than 1000 characters")]
+        [JsonProperty("description")]
         public string Notes { get; set; }
 
         public int Id { get; set; }
