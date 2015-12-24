3cbd676de6a0af96b6c8503ba98e1cf25a80cfef
Peter Velkov <kidroca@gmail.com>
2015-12-24 11:34:52 +0200

    Updated transaction model to have Name and Description

--- Data/FinanceTracker.Data.DbModels/Transactions/Transaction.cs
+++ Data/FinanceTracker.Data.DbModels/Transactions/Transaction.cs
@@ -7,9 +7,15 @@
     {
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(2, ErrorMessage = "Transaction name should be at least 2 characters long")]
+        [MaxLength(128, ErrorMessage = "Transaction name should be less than 128 characters")]
+        public string Name { get; set; }
+
         [Range(0, double.MaxValue, ErrorMessage = "Transaction amount should always be of positive value")]
         public decimal Amount { get; set; }
 
+        [Required]
         public string BalanceId { get; set; }
 
         public virtual Balance Balance { get; set; }
@@ -19,6 +25,10 @@
         public virtual Category Category { get; set; }
 
         public DateTime DateTime { get; set; }
+
+        [MinLength(2, ErrorMessage = "Transaction description should be at least 2 characters long")]
+        [MaxLength(1000, ErrorMessage = "Transaction description should be no longer than 1000 characters")]
+        public string Descriptions { get; set; }
 
         public TransactionType Type { get; set; }
 
